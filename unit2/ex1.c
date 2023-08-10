b818bf342f7c8ea294abd4fe6b4e05695e91eef8
omar badr <oabadr55@gmail.com>
2023-08-10 14:28:00 +0300

    ex1.c

---
+++ unit2/ex1.c
@@ -0,0 +1,26 @@
+/*
+ * ex1.c
+ *
+ *  Created on: Aug 10, 2023
+ *      Author: oabad
+ */
+
+
+/*
+ * ex3.c
+ *
+ *  Created on: Aug 10, 2023
+ *      Author: oabad
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+
+	printf("C programming");
+
+
+}
