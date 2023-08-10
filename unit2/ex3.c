eab536a0d6bc3013f6abcf8b8a44b9de031f400d
omar badr <oabadr55@gmail.com>
2023-08-10 13:59:44 +0300

    ex3.c

---
+++ unit2/ex3.c
@@ -0,0 +1,23 @@
+/*
+ * ex3.c
+ *
+ *  Created on: Aug 10, 2023
+ *      Author: oabad
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	int x,y;
+	printf("enter two intgers :");
+	fflush(stdout);
+	scanf("%d",&x);
+	fflush(stdin);
+	scanf("%d",&y);
+
+	printf("the sum is : %d",x+y);
+
+}
