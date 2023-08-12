dc6ed86d3e149dd2ef7655d0b1655c18fc720524
omar badr <oabadr55@gmail.com>
2023-08-12 15:19:55 +0300

    ex4

--- unit2_homework2/src/ex4Hm2.c
+++ unit2_homework2/src/ex4Hm2.c
@@ -8,4 +8,20 @@
 
 int main(void) {
 
+	double x;
+	printf("enter a number : ");
+	fflush(stdout);fflush(stdin);
+	scanf(" %lf",&x);
+	if(x>0) {
+	printf("%lf is postive",x);
+
+	}
+	else if (x<0.00){
+	printf("%lf is negative",x);
+	}
+
+
+	else
+		printf("the number is zero");
+
 }
